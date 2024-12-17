e43d2a25eda2168e43503d2d4854a66d4f98b232
Tim Holum <tholum@coulee.tech>
2024-12-16 21:21:17 -0600

    Better upload error handlign

--- nodes/PandocConvert/PandocConvert.node.ts
+++ nodes/PandocConvert/PandocConvert.node.ts
@@ -37,7 +37,7 @@ export class PandocConvert implements INodeType {
             },
         },
         inputs: ['main'],
-        outputs: [NodeConnectionType.AiTool , 'main'],
+        outputs: [ 'main'],
         properties: [],
     };
 
