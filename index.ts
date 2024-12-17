893c904979fabe531c57552b8cf9953212a57385
Tim Holum <tholum@coulee.tech>
2024-12-16 21:08:23 -0600

    fix

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-import { INodeType } from 'n8n-workflow';
 import { PandocConvert } from './nodes/PandocConvert/PandocConvert.node';
 
-export const nodes: INodeType[] = [
-	new PandocConvert(),
-];
-
-export const nodeTypes = nodes;
+export { PandocConvert };
